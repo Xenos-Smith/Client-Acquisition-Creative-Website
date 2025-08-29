6191d10ffc1ce165659677c6ea1746d3c0baeb88
Gonçalo Gonçalves <xenossmith1@gmail.com>
2025-08-29 06:21:28 -0700

    Almost there

--- simplesvelte5app/src/routes/Trainings/tool-videos/+layout.svelte
+++ simplesvelte5app/src/routes/Trainings/tool-videos/+layout.svelte
@@ -9,6 +9,10 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children?.()}
+<div class="w-full flex flex-col justify-center items-center">
+	<div class="max-w-screen-xl w-full flex flex-col justify-center items-center gap-10 mt-20 mb-20">
+	{@render children?.()}
+	</div>
+</div>
 
 <!--create video series styling & others-->
